734bb20ab9434c155683915507124ab2e3364d25
Hugo Feitosa <hugofeitosa93@gmail.com>
2020-10-21 17:36:30 -0300

    insercao

---
+++ urna/etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '55000',
+                nome: 'Tiringa',
+                partido: 'PCS',
+                fotos: [
+                    {url:'55000.jpg', legenda: 'Vereador', small: false}
+                ]
+            },
+            {
+                numero: '42999',
+                nome: 'Charlles Rekson',
+                partido: 'PBS',
+                fotos: [
+                    {url:'42999.jpg', legenda: 'Vereador', small: false}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '40',
+                nome: 'Geraldo',
+                partido: 'PSB',
+                vice: 'João da Costa',
+                fotos: [
+                    {url: '40.jpg', legenda: 'Prefeito'},
+                    {url: '40_2.jpg', legenda: 'Vice-Prefeito', small:true}
+                ]
+            },
+            {
+                numero: '19',
+                nome: 'Daniel Coelho',
+                partido: 'PODEMOS',
+                vice: 'Isabela de Roldão',
+                fotos: [
+                    {url: '19.jpg', legenda: 'Prefeito'},
+                    {url: '19_2.jpg', legenda: 'Vice-Prefeito', small:true}
+                ]
+            },
+        ]
+    }
+];
